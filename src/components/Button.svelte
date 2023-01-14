e376eaa521e825825829bd26f2fc0f63c5fe4437
raverena <raverena96@gmail.com>
2023-01-14 18:19:57 +0900

    [Init] project

---
+++ src/components/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	const handleClick = () => {
+		dispatch('click', {
+			text: 'hello'
+		});
+	};
+</script>
+
+<button on:click={handleClick}>
+	<slot />
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let exercises = [
+		{ exercise: '스쿼트', weight: 0 },
+		{ exercise: '벤치프레스', weight: 0 },
+		{ exercise: '데드리프트', weight: 0 }
+	];
+</script>
+
+{#each exercises as { exercise, weight } (exercise)}
+	<label>
+		{exercise}
+		<input type="number" name={exercise} bind:value={weight} />
+	</label>
+{/each}
+
+{#each exercises as { exercise, weight } (exercise)}
+	<div>{exercise} : {weight}</div>
+{/each}
