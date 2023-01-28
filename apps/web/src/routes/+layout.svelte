4bd1cf46d5e3288b5a9e0fd73acecf858955890c
raverena <raverena96@gmail.com>
2023-01-28 16:51:57 +0900

    [Add] modal

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
 	import '../app.css'; // tailwind
 	import Icon from '@components/Icon.svelte';
+	import Modal from '@components/Modal.svelte';
 
 	let tabs = [
 		{ name: 'home', icon: 'home' },
@@ -9,6 +10,14 @@
 		{ name: 'graph', icon: 'graph' },
 		{ name: 'social', icon: 'social' }
 	];
+
+	let showModal = false;
+
+	const handleTabClick = (name: string) => {
+		if (name === 'exercise') {
+			showModal = true;
+		}
+	};
 </script>
 
 <div id="background" class="flex h-screen w-screen items-center justify-center bg-slate-50">
@@ -26,17 +35,29 @@
 				class="flex h-14 w-full max-w-xs items-center justify-center rounded-lg bg-white drop-shadow-md"
 			>
 				{#each tabs as tab (tab.name)}
-					<div
+					<button
 						class={`flex h-full w-full flex-1 items-center justify-center rounded-lg hover:cursor-pointer ${
 							tab.name === 'exercise'
 								? 'bg-primary hover:bg-primary-dark'
 								: 'bg-white hover:bg-gray-50'
 						}`}
+						on:click={() => handleTabClick(tab.name)}
 					>
 						<Icon icon={tab.icon} />
-					</div>
+					</button>
 				{/each}
 			</div>
 		</div>
 	</div>
 </div>
+
+{#if showModal}
+	<Modal on:close={() => (showModal = false)}>
+		<h2 slot="header">
+			modal
+			<small><em>adjective</em> mod·al \ˈmō-dəl\</small>
+		</h2>
+
+		<div />
+	</Modal>
+{/if}
